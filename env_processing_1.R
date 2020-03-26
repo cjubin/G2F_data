b58f70ffd051faff65ca43021c51a38dc24edf63
cathyjubin <cathyjubin@DNPW-PZ-68n.agrar.uni-goettingen.de>
2020-03-26 10:17:43 +0100

    modif revgeo

--- env_processing_1.R
+++ env_processing_1.R
@@ -9,8 +9,10 @@ library(intrval)
 library(dplyr)
 library(ggplot2)
 library(rnoaa)
+library(ggmap)
 library(revgeo)
 library(FedData)
+source('latlong2county.R')
 `%notin%` <- Negate(`%in%`)
 
 ##Import 5 years of weather data
@@ -65,9 +67,9 @@ rm(weather2014,weather2014bis,weather2015,weather2015bis,weather2016,weather2016
 
 ##################################################################################
 ####Loading the field metadata files: geographical coordinates and name cities etc
-####Attribute Lon/Lat data using field metadata files + name cities + name counties for each field experiment 
 setwd("~/Final_datasets_G2F/ALL_WEATHER")
 field2014=read.csv('g2f_2014_field_characteristics.csv',header = T,sep = ',')
+field2014=field2014[-which(field2014$Type=='inbred'),]
 field2015=read.csv('g2f_2015_field_metadata.csv',header = T,sep = ',')
 field2016=read.csv('g2f_2016_field_metadata.csv',header = T,sep = ',',stringsAsFactors=FALSE, fileEncoding="latin1")
 field2017=read.csv('g2f_2017_field_metadata.csv',header = T,sep = ',',stringsAsFactors=FALSE, fileEncoding="latin1")
@@ -78,20 +80,20 @@ field2016$Year=2016
 field2017$Year=2017
 field2018$Year=2018
 
-#Retrieving Lon/Lat data for each field experiment
-names_col2=c('lat','long')
-colnames(field2015)[c(6,7)]=c('lat','long')
-colnames(field2016)[c(9,10)]=c('lat','long')
+#Retrieving Lon/Lat data for each field experiment --> use of the field corner instead of the WS coordinates
+#For retrieving soil data: more accurate to use field than weather location, which can be very different
+#If the Weather station had dubious data, then we will use a nearby location to the field instead of one near to the weather station.
+colnames(field2015)[c(19,20)]=c('lat','long')
+colnames(field2016)[c(23,24)]=c('lat','long')
 colnames(field2016)[1]='Experiment'
-colnames(field2017)[c(9,10)]=c('lat','long')
+colnames(field2017)[c(26,27)]=c('lat','long')
 colnames(field2017)[1]='Experiment'
-colnames(field2018)[c(9,10)]=c('lat','long')
+colnames(field2018)[c(26,27)]=c('lat','long')
 colnames(field2018)[1]='Experiment'
-field2018[field2018$Experiment=='WIH2',c('lat','long')]=c('43.324231'	,'-89.33564')
-field2015[field2015$Experiment=='WIH1',c('lat','long')]=c('43.05443'	,'-89.52875')
-field2016[field2016$Experiment=='INH1',c('lat','long')]=c('40.47925'	,'-86.99013')
+
+
 geofield2014=field2014[,c('lat','long','Year','Experiment','City')]
-geofield2015=field2015[,c('lat','long','Year','Experiment','City')]##NYH1 disease trial with no info, TXH2 missing --> need to be removed, absolutely no info on this trial
+geofield2015=field2015[,c('lat','long','Year','Experiment','City')]
 geofield2016=field2016[,c('lat','long','Year','Experiment','City')]
 geofield2017=field2017[,c('lat','long','Year','Experiment','City')]
 geofield2018=field2018[,c('lat','long','Year','Experiment','City')]
@@ -99,7 +101,18 @@ geofield2018[c(15,16),'Experiment']='MOH1'
 geo_data_fields=rbind(geofield2014,geofield2015,geofield2016,geofield2017,geofield2018)
 colnames(geo_data_fields)[4]='Field.Location'
 
-##Merge weather data with geographical coordinates
+#Missing coordinates 
+geo_data_fields[82,c('lat','long')]=c(41.19869,	-91.48620)
+geo_data_fields[c(30,83),c('lat','long')]=c(42.06591,	-94.72745)
+geo_data_fields[c(84),c('lat','long')]=c(41.98738,	-92.26016)
+geo_data_fields[c(85),c('lat','long')]=c(41.99750,	-93.69990)
+
+#Use revgeo to add county and city
+results_couties<-latlong2county(data.frame(geo_data_fields$long[!is.na(geo_data_fields$long)],geo_data_fields$lat[!is.na(geo_data_fields$lat)]))
+results_cities=revgeo(longitude=geo_data_fields$long[!is.na(geo_data_fields$long)], latitude=geo_data_fields$lat[!is.na(geo_data_fields$lat)], provider = 'photon',item='city', output='frame')
+
+
+#Merge weather data with geographical coordinates
 weather=merge(geo_data_fields,weather_all,by=c('Year','Field.Location'), all.y = T)##Merging the weather data with Lon/lat + name cities 
 rm(weather_all)
 
@@ -118,6 +131,7 @@ row.names(missing_lonlat)=NULL
 #2014_NEH3 ,2017_NEH3 and 2017_NEH4 have no silking date
 #2015_NEH1 has no info on irrigation, 2015_NEH2 has no plant height, 2015_NEH3 has no plant height and  2015_NEH4 has no silking date
 #SCH1: no grain yield data
+##NYH1 disease trial with no info, TXH2 missing --> need to be removed, absolutely no info on this trial
 weather$Year_Exp=paste(weather$Year,weather$Field.Location,sep = '_')
 to_remove=missing_lonlat$Year_Exp[c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,29,30)]
 weather=weather[-which(weather$Year_Exp%in%to_remove),]
