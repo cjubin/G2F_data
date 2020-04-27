ec9918e256485a2463fba97dac522e489d904e34
cathyjubin <cathyjubin@DNPW-PZ-68n.agrar.uni-goettingen.de>
2020-04-27 11:57:06 +0200

    modifications names parameters

--- impute_kriging_withGHCND.R
+++ impute_kriging_withGHCND.R
@@ -3,7 +3,7 @@
 #' \code{impute_kriging_withGHCND} interpolates values for a specific meteorological variable given a time frame for a specific location
 #' @param Year_Exp Character. Experiment (associated iwth a specific field location) in the G2F dataset which needs to be imputed.
 #' @param radius Numeric. Distance from the field location to consider to interpolate.
-#' @param meteo_variable Character. GHCND element names: 
+#' @param 
 #' 
 #' 
 #' 
@@ -15,7 +15,7 @@
 #' 
 
 
-impute_kriging_withGHCND <- function(Year_Exp,radius=100,meteo_variable,daily_weather=daily_weather) {
+impute_kriging_withGHCND <- function(Year_Exp,radius=70,meteo_variable_GHCND,daily_weather=daily_weather,meteo_variable_in_table=NULL) {
   library(rnoaa)
   library(raster)
   library(mapdata)
@@ -38,11 +38,13 @@ impute_kriging_withGHCND <- function(Year_Exp,radius=100,meteo_variable,daily_we
   latitude=as.numeric(unique(daily_weather[daily_weather$Year_Exp==Year_Exp,'lat'])[!is.na(unique(daily_weather[daily_weather$Year_Exp==Year_Exp,'lat']))])
   
   #Values recorded for this variable at the field station
-  field_values=daily_weather[daily_weather$Year_Exp==Year_Exp,meteo_variable]
+  if (!is.null(meteo_variable_in_table)) {
+    field_values = daily_weather[daily_weather$Year_Exp == Year_Exp, meteo_variable_in_table]
+  }
   
   #Finding the closest stations in a certain radius and select lines (stations) with TEMP data
   stations_close=as.data.frame(meteo_distance(stations,latitude,longitude,radius = radius))
-  stations_close<-filter(stations_close,element%in%meteo_variable)
+  stations_close<-filter(stations_close,element%in%meteo_variable_GHCND)
   stations_close<-arrange(stations_close,distance)
   
   #Download data from stations exhibiting the meteo variable of interest
@@ -65,7 +67,7 @@ impute_kriging_withGHCND <- function(Year_Exp,radius=100,meteo_variable,daily_we
   
   
   
-  all_data=lapply(stations_close$id, function(x)download_data(x,datatypeid = meteo_variable))
+  all_data=lapply(stations_close$id, function(x)download_data(x,datatypeid = meteo_variable_GHCND))
   
   all_data<-plyr::compact(all_data)
   all_data=as.data.frame(do.call('rbind',all_data))
@@ -123,12 +125,12 @@ impute_kriging_withGHCND <- function(Year_Exp,radius=100,meteo_variable,daily_we
   
   pred<-krigeST(values~1,data=timeDF,modelList =fitted.stvgm,newdata = grid.ST )
   predicted.values=pred@data$var1.pred
-  cors=cor(predicted.values,field_values,use = 'complete.obs')
-  
+  if (!is.null(meteo_variable_in_table)) {
+    cors=cor(predicted.values,field_values,use = 'complete.obs')}
   
   dates=seq(as.Date(date_start,tz="CET"),as.Date(date_end,tz="CET"),by='days')
   predictions.table=cbind(Year_Exp,predicted.values,as.character(dates))
-  colnames(predictions.table)=c('Year_Exp',paste(meteo_variable),'dates')
+  colnames(predictions.table)=c('Year_Exp',paste(meteo_variable_GHCND),'dates')
   return(list(predictions.table,cors))
   
   
