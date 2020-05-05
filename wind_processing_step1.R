51b6537e38a7012695e46d4113b87dd17b454dd7
cathyjubin <cathyjubin@DNPW-PZ-68n.agrar.uni-goettingen.de>
2020-05-05 15:17:28 +0200

    wind processing

--- wind_processing_step1.R
+++ wind_processing_step1.R
@@ -19,7 +19,7 @@ library(rgdal)
 
 
 library(dplyr)
-#library(plyr)
+library(plyr)
 library(lubridate)
 
 # ------------------------------------------------------------------------------
@@ -45,91 +45,107 @@ daily_weather = read.table(
   stringsAsFactors = F
 )
 # ------------------------------------------------------------------------------
-##HUMIDITY: flagged values to be imputed
+##WIND: flagged values to be imputed
 # ------------------------------------------------------------------------------
 ##Control range semi-hourly or 15-, 20-minutes data values
-weather$flagged_humidity_instant=NA
-weather[which(weather$Relative.Humidity....>100),'flagged_humidity_instant']<-'flagged'
-weather[which(weather$Relative.Humidity....>100),'Relative.Humidity....']<-NA
-weather[which(weather$Relative.Humidity....<0),'flagged_humidity_instant']<-'flagged'
-weather[which(weather$Relative.Humidity....<0),'Relative.Humidity....']<-NA
+weather$flagged_WIND_instant=NA
+weather$Wind.Speed..m.s.=as.numeric(weather$Wind.Speed..m.s.)
+weather[which(weather$Wind.Speed..m.s.>75),'flagged_WIND_instant']<-'flagged'
+weather[which(weather$Wind.Speed..m.s.>75),'Wind.Speed..m.s.']<-NA
+weather[which(weather$Wind.Speed..m.s.<0),'flagged_WIND_instant']<-'flagged'
+weather[which(weather$Wind.Speed..m.s.<0),'Wind.Speed..m.s.']<-NA
 
-#First flagged value assignment based on ratio (humidity values present)/(nb obs per day) >0.9 
+# ------------------------------------------------------------------------------
+#Internal consistency test 1)wind speed = 00 and wind direction = 00 and 2)wind speed ≠ 00 and wind direction ≠ 00:
+# ------------------------------------------------------------------------------
+weather[which(weather$Wind.Direction..degrees.==0&weather$Wind.Speed..m.s.!=0),'flagged_WIND_instant']<-'flagged'
+weather[which(weather$Wind.Direction..degrees.==0&weather$Wind.Speed..m.s.!=0),'Wind.Speed..m.s.']<-NA
+
+weather[which(weather$Wind.Direction..degrees.!=0&weather$Wind.Speed..m.s.==0),'flagged_WIND_instant']<-'flagged'
+weather[which(weather$Wind.Direction..degrees.!=0&weather$Wind.Speed..m.s.==0),'Wind.Speed..m.s.']<-NA
+
+
+
+#First flagged value assignment based on ratio (WIND values present)/(nb obs per day) >0.9 
 weather$Year_Exp=as.factor(weather$Year_Exp)
 weather$Day.of.Year=as.factor(weather$Day.of.Year)
 total_obs_daily<-as.data.frame(weather %>% group_by(Year_Exp,Day.of.Year) %>% tally())
 
-nomissing_humidity<-weather %>% 
-  aggregate(Relative.Humidity....~Day.of.Year+Year_Exp,data=.,FUN = (function(x) {sum(!is.na(x))})) 
-colnames(nomissing_humidity)[3]='number_obs_present'
+nomissing_WIND<-weather %>% 
+  aggregate(Wind.Speed..m.s.~Day.of.Year+Year_Exp,data=.,FUN = (function(x) {sum(!is.na(x))})) 
+colnames(nomissing_WIND)[3]='number_obs_present'
 
-alldaymissing_humidity<-weather %>% 
+alldaymissing_WIND<-weather %>% 
   group_by(Day.of.Year,Year_Exp)%>%
-  dplyr::summarise(all(is.na(Relative.Humidity....)))
-colnames(alldaymissing_humidity)[3]='all_missing_humidity'
+  dplyr::summarise(all(is.na(Wind.Speed..m.s.)))
+colnames(alldaymissing_WIND)[3]='all_missing_WIND'
 
 
 
-humidity<-merge(total_obs_daily,nomissing_humidity,by=c('Day.of.Year','Year_Exp'),all.x=T)
-humidity<-merge(humidity,alldaymissing_humidity,by=c('Day.of.Year','Year_Exp'),all.x=T)
+WIND<-merge(total_obs_daily,nomissing_WIND,by=c('Day.of.Year','Year_Exp'),all.x=T)
+WIND<-merge(WIND,alldaymissing_WIND,by=c('Day.of.Year','Year_Exp'),all.x=T)
 
-rm(total_obs_daily,nomissing_humidity,alldaymissing_humidity)
+rm(total_obs_daily,nomissing_WIND,alldaymissing_WIND)
 
-humidity<-humidity%>%group_by(Day.of.Year,Year_Exp)%>%mutate(ratio=number_obs_present/n)%>%mutate(flagged_humidity=case_when(ratio<0.9 ~'flagged',
+WIND<-WIND%>%group_by(Day.of.Year,Year_Exp)%>%mutate(ratio=number_obs_present/n)%>%mutate(flagged_WIND=case_when(ratio<0.9 ~'flagged',
                                                                                                                              ratio>=0.9 ~'OK'))
+
+
 # ------------------------------------------------------------------------------
 #Weird number of daily observations recorded (ex: 50, 100 etc and not 24, 48, 72) then the value is flagged too.
 # ------------------------------------------------------------------------------
 
-humidity[humidity$n<24,'flagged_humidity']='flagged'
-humidity[humidity$n>96,'flagged_humidity']='flagged'
-humidity[humidity$n>24&humidity$n<48,'flagged_humidity']='flagged'
-humidity[humidity$n>48&humidity$n<72,'flagged_humidity']='flagged'
-humidity[humidity$n>72&humidity$n<96,'flagged_humidity']='flagged'
-humidity[humidity$all_missing_humidity==TRUE,'flagged_humidity']='flagged'
-humidity[humidity$all_missing_humidity==TRUE,'ratio']=0
+WIND[WIND$n<24,'flagged_WIND']='flagged'
+WIND[WIND$n>96,'flagged_WIND']='flagged'
+WIND[WIND$n>24&WIND$n<48,'flagged_WIND']='flagged'
+WIND[WIND$n>48&WIND$n<72,'flagged_WIND']='flagged'
+WIND[WIND$n>72&WIND$n<96,'flagged_WIND']='flagged'
+WIND[WIND$all_missing_WIND==TRUE,'flagged_WIND']='flagged'
+WIND[WIND$all_missing_WIND==TRUE,'ratio']=0
 
-weather=merge(weather,humidity[,c(1:2,7)],by=c('Day.of.Year','Year_Exp'),all.x=T)
+weather=merge(weather,WIND[,c(1:2,7)],by=c('Day.of.Year','Year_Exp'),all.x=T)
 
-daily_weather=merge(daily_weather,humidity[,c(1:3,7)],by=c('Day.of.Year','Year_Exp'),all.x=T)
+daily_weather=merge(daily_weather,WIND[,c(1:3,7)],by=c('Day.of.Year','Year_Exp'),all.x=T)
+
 
 
 # ------------------------------------------------------------------------------
-#Daily mean,max and min humidity computed for unflagged values:
+#Daily average wind speed computed for unflagged values:
 # ------------------------------------------------------------------------------
 
-maxH<-weather%>%
-  filter(flagged_humidity%in%'OK')%>%
+mean_wind<-weather%>%
+  filter(flagged_WIND%in%'OK')%>%
   group_by(Day.of.Year,Year_Exp)%>%
-  dplyr::mutate(HMAX=max(Relative.Humidity....,na.rm = T))%>%
-  select(Day.of.Year,Year_Exp,HMAX)
-maxH<-unique(maxH)
-minH<-weather%>%
-  filter(flagged_humidity%in%'OK')%>%
-  group_by(Day.of.Year,Year_Exp)%>%
-  dplyr::mutate(HMIN=min(Relative.Humidity....,na.rm = T))%>%
-  select(Day.of.Year,Year_Exp,HMIN)
-minH<-unique(minH)
-allhumidity=merge(maxH,minH,by=c('Day.of.Year','Year_Exp'),all.x=T)
-allhumidity$HMEAN=(allhumidity$HMAX+allhumidity$HMIN)/2
-allhumidity=arrange(allhumidity,Year_Exp,Day.of.Year)
+  dplyr::mutate(MEANWINDSPEED=mean(Wind.Speed..m.s.,na.rm = T))%>%
+  dplyr::select(Day.of.Year,Year_Exp,MEANWINDSPEED)
+mean_wind<-unique(mean_wind)
+
+mean_wind=arrange(mean_wind,Year_Exp,Day.of.Year)
 
 # ------------------------------------------------------------------------------
-#Add not flagged daily computed HMIN, HMAX, HMEAN to the daily_weather table (based on data from the field station)
+#Step test: absolute difference between two consecutive days should be <10 m/s:
 # ------------------------------------------------------------------------------
-daily_weather<-merge(daily_weather,allhumidity,by=c('Day.of.Year','Year_Exp'),all.x = T)
+
+
+test  <- 
+  mean_wind %>%
+  group_by(Year_Exp) %>%
+  mutate(lag = dplyr::lag(MEANWINDSPEED, n = 1, default = NA))%>%
+  mutate(diff = abs(MEANWINDSPEED-lag))%>%
+  filter(diff<10&diff!=0)%>%
+  dplyr::select(Year_Exp,Day.of.Year,MEANWINDSPEED)
+
+mean_wind<-test
+
+# ------------------------------------------------------------------------------
+#Add not flagged daily computed MEANWINDSPEED to the daily_weather table (based on data from the field station)
+# ------------------------------------------------------------------------------
+daily_weather<-merge(daily_weather,mean_wind,by=c('Day.of.Year','Year_Exp'),all.x = T)
 daily_weather=arrange(daily_weather,Year,Year_Exp,Day.of.Year)
 
 
 
 
-
-
-##Note (to add): The preferred method to obtain an average RH over a given interval is to average the vapor pressure and saturation vapor pressures, then calculate RH from those averages
-
-
-
-
 #Write the table which will be used for comparison with interpolated values for non missing values
-write.table(daily_weather,'daily_weather_humidity_processed1.txt',col.names=T,row.names=F,sep='\t',quote=F)
+write.table(daily_weather,'daily_weather_wind_processed1.txt',col.names=T,row.names=F,sep='\t',quote=F)
 
